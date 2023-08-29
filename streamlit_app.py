8f9ee6ecf61ddd9ff2731205edc0f38749921683
rbocelo <143390043+rbocelo@users.noreply.github.com>
2023-08-29 19:39:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ if streamlit.button ('Get Fruit Load List'):
   my_data_rows = get_fruit_list ()
   streamlit.dataframe(my_data_rows) 
 
-streamlit.stop ()
+#streamlit.stop ()
 
 def insert_row_snowflake (new_fruit):
   with my_cnx.crusor() as my_cur:
