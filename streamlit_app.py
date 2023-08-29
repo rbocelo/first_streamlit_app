9c548bc65d576c50f07f56584260ca3ee92e73f6
rbocelo <143390043+rbocelo@users.noreply.github.com>
2023-08-29 19:23:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ def get_fruit_list ():
         my_cur.execute("SELECT * from fruit_load_list")
         return = my_cur.fetchall()
 
-if streamlit.botton ('Get Fruit Load List'):
+if streamlit.button ('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_list ()
   streamlit.dataframe(my_data_rows) 
